fd279b760d9413be86a23d838381eac57975771c
jordi007 <jordi007@users.noreply.github.com>
2017-04-23 14:43:42 -0600

    Plantilla basica

---
+++ spoology/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Spoology</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/master.css">
+</head>
+<body>
+<!-- navbar-default #252D49 - style="color: #252D49" style="background-color: #252D49; color: white;"-->
+<?php 
+  include_once("menu.php");
+?>
+<!-- comienzo de los div -->
+<div class="container-fluid">
+  <div class="row">
+     <div style="padding: 5px;" id="opciones" class="col-lg-2">
+       <div style="background-color: white; height: absolute; border-color: white;" class="well">
+          algo1
+          <br><br><br><br><br><br>gasparin <br><br><br><br><br>tata tata taaaa
+       </div> 
+     </div>
+     <div style="padding: 5px;" id="contenido" class="col-lg-7">
+       <div class="well">
+          algo2
+       </div>
+     </div>
+     <div style="padding: 5px;" id="pendientes" class="col-lg-3">
+       <div class="well">
+          algo3
+       </div>
+     </div>
+  </div>
+</div>
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ spoology/menu.php
@@ -0,0 +1,130 @@
+<nav class="navbar" >
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header" >
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a style="color: white;" class="navbar-brand" href="#" >Spoology</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="dropdown">
+          <a style="color: white" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true"> <span class="glyphicon glyphicon-search" aria-hidden="true"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#"><input type="text" id="" name="" class="form-control" placeholder="Buscar"></a></li>  
+          </ul>
+        </li>
+        <li><a href="#" style="color: white">Inicio</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" style="color: white">Cursos <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">algun curso</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">algun curso</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">algun curso</a></li>
+            <li role="separator" class="divider"></li>
+	        <div id="" class="">
+	            <span >
+	            	<a href="#" >Unirse</a>
+	            </span>
+
+	            <span  >
+	            	<a href="#" >Ver todos</a>
+	            </span>	
+	        </div> 
+          </ul>
+        </li>
+
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" style="color: white">Grupos <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">algun grupo</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">algun grupo</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">algun grupo</a></li>
+            <li role="separator" class="divider"></li>
+	        <div id="" class="">
+	            <span >
+	            	<a href="#" >Unirse</a>
+	            </span>
+	            
+	            <span  >
+	            	<a href="#" >Explorar</a>
+	            </span>	
+	        </div> 
+          </ul>
+        </li>
+
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" style="color: white">Recursos <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#"> # Personal</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#"> # Grupo</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#"> # Apps</a></li>
+            <li role="separator" class="divider"></li> 
+          </ul>
+        </li>
+
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li>
+          <a style="color: white" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span></a>
+          <ul class="dropdown-menu">
+            <div>Solicitudes</div>
+            <li><a>Solicitudes</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+        <li>
+          <a style="color: white" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></a>
+          <ul class="dropdown-menu">
+            <div>Solicitudes</div>
+            <li><a>Solicitudes</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+        <li>
+          <a style="color: white" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-stats" aria-hidden="true"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+        
+        <li><a style="color: white">Nombre del individuo</a></li>
+        <li class="dropdown">
+          <a style="color: white" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+        <li><a style="color: white" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true"> <span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span></a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
