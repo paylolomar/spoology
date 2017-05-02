cb383479f56cc1caf005663076185e329c7f8b3e
jordi007 <jordi007@users.noreply.github.com>
2017-05-02 15:41:36 -0600

    plantilla de verificacion de dodigo de acceso

---
+++ verificacion-de-codigo.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Verificacion de codigo</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/master.css">
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+<div class="container-fluid">
+	<div class="row">
+	<?php 
+		include_once("menu2.php");
+	?>
+		<div class="col-lg-5 col-sm-5 col-xs-5 col-md-5"></div>
+		<div class="col-lg-2 col-sm-2 col-xs-2 col-md-2">
+			<div>
+				<h3><strong>Sign up for Spoology</strong></h3>
+				<input type="text" name="" id="txt-confirmar-codigo" class="form-control" placeholder="Access Code">
+			</div>
+		</div>
+		<div class="col-lg-5 col-sm-5 col-xs-5 col-md-5"></div>
+	</div>
+</div>
+<footer id="page-footer" style="position: relative;">
+  <div class="container">
+    <div id="copyright-and-links">
+      Spoology &copy;
+      2017        <span role="separator" class="divider">&middot;</span>
+        <a id="footer-nav-privacy" href="https://www.schoology.com/privacy" target="_blank">Privacy Policy</a>
+        <span class="divider">&middot;</span>
+        <a id="footer-nav-privacy" href="https://www.schoology.com/terms-of-use" target="_blank">Terms of Use</a>
+      <span class="divider">&middot;</span>
+      <a id="footer-nav-help-center" href="https://support.schoology.com/hc/en-us" rel="external" target="_blank">Help Center</a> 
+    </div>
+    <div id="trust" style="" align="right">
+      <a href="https://www.schoology.com/privacy" target="_blank" id="privacy-link">
+        <img alt="Privo Privacy Certified" src="https://privo.com/images/certLogos/H_PPC_B.png" />
+      </a>
+    </div>
+  </div>
+</footer>
+</body>
+</html>
