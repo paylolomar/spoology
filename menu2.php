032b92e4b22a026c7666235f7ee8d87cb9d20b7f
jordi007 <jordi007@users.noreply.github.com>
2017-05-02 15:40:35 -0600

    menu sin funcionalidad

---
+++ menu2.php
@@ -0,0 +1,10 @@
+	<nav class="navbar" style="background-color: rgb(37, 45, 73);">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header" >
+		    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			</button>
+	      <a style="color: white; margin-left: 250px;" class="navbar-brand" href="#" ><span style="color: blue;" class="glyphicon glyphicon-usd" aria-hidden="true"></span> Spoology</a>
+	   </div> <!-- Collect the nav links, forms, and other content for toggling -->
+	  </div><!-- /.container-fluid -->
+	</nav>
