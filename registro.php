3fcd2b2713a18c419f145b31feaff1397e0312eb
jordi007 <jordi007@users.noreply.github.com>
2017-05-02 15:41:00 -0600

    registro

---
+++ registro.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registro</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/master.css">
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+<div class="container-fluid">
+	<div class="row">
+			<div>
+				<?php 
+					include_once("menu2.php");
+				 ?>
+			</div>
+		<div class="col-lg-2 col-sm-2 col-xs-2 col-md-2"></div>
+		<div id="cuadro"  class="well col-lg-4 col-sm-4 col-xs-4 col-md-4" > 
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 col-sm-12 col-xs-12 col-md-12">
+					 <h4 style="margin-left: 40px;"><strong>Sign up for Spoology</strong></h4>
+					 <a style="text-align: right;" href="#" class="btn" >Back</a>
+						<ul>
+							
+								<a class="btn" id="btn-instructor" style="color: #000000; font-size: 30px; text-align: center; background-color: #7ed0f7" href="www.google.com">Instructor</a>
+							
+							
+								<a class="btn" id="btn-estudiante" style="color: #000000; font-size: 30px; text-align: center; background-color: #7ed0f7; margin-left: 40px;" href="">Student</a>
+							<br>
+							
+								<a class="btn" id="btn-familiares" style="background-color: #c9c3c3; margin-left: 130px; margin-top: 10px; text-align: center; font-size: 20px; background-size: 30px;" href="">Parent</a>
+							
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-2 col-sm-2 col-xs-2 col-md-2"></div>
+	</div>
+</div>
+<footer id="page-footer">
+  <div class="container">
+    <div id="copyright-and-links">
+      Spoology &copy;
+      2017        <span role="separator" class="divider">&middot;</span>
+        <a id="footer-nav-privacy" href="https://www.schoology.com/privacy" target="_blank">Privacy Policy</a>
+        <span class="divider">&middot;</span>
+        <a id="footer-nav-privacy" href="https://www.schoology.com/terms-of-use" target="_blank">Terms of Use</a>
+      <span class="divider">&middot;</span>
+      <a id="footer-nav-help-center" href="https://support.schoology.com/hc/en-us" rel="external" target="_blank">Help Center</a> 
+    </div>
+    <div id="trust" style="" align="right">
+      <a href="https://www.schoology.com/privacy" target="_blank" id="privacy-link">
+        <img alt="Privo Privacy Certified" src="https://privo.com/images/certLogos/H_PPC_B.png" />
+      </a>
+    </div>
+  </div>
+</footer>
+</body>
+</html>
