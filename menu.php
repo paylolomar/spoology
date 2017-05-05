15c75ef904fccc4178c70157f5aa1a36bb5b1e68
jordi007 <jordi007@users.noreply.github.com>
2017-05-05 01:53:51 -0600

    sign out

--- menu.php
+++ menu.php
@@ -118,7 +118,7 @@
             <li><a href="#">Another action</a></li>
             <li><a href="#">Something else here</a></li>
             <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
+            <li><a href="cerrar-sesion.php">Sing out</a></li>
           </ul>
         </li>
         <li><a style="color: white" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true"> <span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span></a></li>
