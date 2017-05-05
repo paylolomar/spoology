4bdc4eb425ded0c9c57548aef221eb53e26e6069
jordi007 <jordi007@users.noreply.github.com>
2017-05-05 03:50:03 -0600

    seg

--- menu_cuenta.php
+++ menu_cuenta.php
@@ -1,8 +1,8 @@
-
 <?php session_start(); 
 	if(!isset($_SESSION['codigo_usuario']))
 		header("Location: index.php");
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Spoology</title>
