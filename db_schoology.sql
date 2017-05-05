385d7ee0a5a6081a4ac86458155b9d844bb76f74
Gabriel Mejía <paylolomar@gmail.com>
2017-05-05 06:48:28 -0600

    qwe
    asd

--- db_schoology.sql
+++ db_schoology.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-05-2017 a las 05:21:06
+-- Tiempo de generación: 05-05-2017 a las 12:47:48
 -- Versión del servidor: 5.7.14
 -- Versión de PHP: 5.6.25
 
--- home.php
+++ home.php
@@ -1,6 +1,6 @@
-<?php session_start(); 
-	if(!isset($_SESSION['codigo_usuario']))
-		header("Location: index.php");
+<?php //session_start(); 
+	//if(!isset($_SESSION['codigo_usuario']))
+		//header("Location: index.php");
 ?>
 <!DOCTYPE html>
 <html>
